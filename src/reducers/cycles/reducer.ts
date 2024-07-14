8ddeeb738de3a9c1287698755296bcb2f7ad2645
breno-okra <brenookra@gmail.com>
2024-07-14 20:53:08 -0300

    using immer in actionTypes for be mutavies

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,3 +1,5 @@
+import { ActionTypes } from "./actions";
+import { produce } from "immer"
 export interface Cycle {
     id: string;
     task: string;
@@ -13,43 +15,66 @@ interface CycleState {
 //Enums permitem que você defina um conjunto de valores que representam um tipo específico de dado,
 //tornando seu código mais claro e compreensível. 
 //Ao utilizar enums, você pode criar um conjunto de constantes que são semanticamente relevantes.
-export enum ActionTypes {
-    ADD_NEW_CYCLE = "ADD_NEW_CYCLE",
-    INTERRUPT_CURRENT_CYCLE = "INTERRUPT_CURRENT_CYCLE",
-    MARK_CURRENT_CYCLE_AS_FINISHED = "MARK_CURRENT_CYCLE_AS_FINISHED"
-}
-export function cyclesReducer(state: CycleState, action){
-    switch(action.type){
+
+export function cyclesReducer(state: CycleState, action: any) {
+    switch (action.type) {
         case ActionTypes.ADD_NEW_CYCLE:
-            return {
-                ...state,
-                cycles: [...state.cycles, action.payload.newCycle],
-                activeCycleId: action.payload.newCycle.id
+            /*  return {
+                 ...state,
+                 cycles: [...state.cycles, action.payload.newCycle],
+                 activeCycleId: action.payload.newCycle.id
+             } */
+            return produce(state, draft => {
+                draft.cycles.push(action.payload.newCycle)
+                draft.activeCycleId = action.payload.newCycle.id
+            })
+        case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
+            /*  return{
+                 ...state,
+                 cycles:state.cycles.map((cycle) => {
+                     if (cycle.id === state.activeCycleId) {
+                         return { ...cycle, interruptedDate: new Date() }
+                     } else {
+                         return cycle;
+                     }
+                 }),
+                 activeCycleId:null
+             } */
+            const currentCycleIndex = state.cycles.findIndex(cycle => {
+                return cycle.id == state.activeCycleId
+            })
+            if (currentCycleIndex < 0) {
+                return state
             }
-        case ActionTypes.INTERRUPT_CURRENT_CYCLE:
-            return{
-                ...state,
-                cycles:state.cycles.map((cycle) => {
-                    if (cycle.id === state.activeCycleId) {
-                        return { ...cycle, interruptedDate: new Date() }
-                    } else {
-                        return cycle;
-                    }
-                }),
-                activeCycleId:null
+            return produce(state, draft => {
+
+                draft.activeCycleId = null
+                draft.cycles[currentCycleIndex].interruptedDate = new Date()
+            })
+        }
+        case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:{
+            /*  return{
+                 ...state,
+                 cycles:state.cycles.map((cycle) => {
+                     if (cycle.id === state.activeCycleId) {
+                         return { ...cycle, finishedDate: new Date() };
+                     } else {
+                         return cycle;
+                     }
+                 }),
+                 activeCycleId:null
+             } */
+            const currentCycleIndex = state.cycles.findIndex(cycle => {
+                return cycle.id == state.activeCycleId
+            })
+            if (currentCycleIndex < 0) {
+                return state
             }
-        case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
-            return{
-                ...state,
-                cycles:state.cycles.map((cycle) => {
-                    if (cycle.id === state.activeCycleId) {
-                        return { ...cycle, finishedDate: new Date() };
-                    } else {
-                        return cycle;
-                    }
-                }),
-                activeCycleId:null
-            }
+            return produce(state, draft => {
+
+                draft.activeCycleId = null
+                draft.cycles[currentCycleIndex].finishedDate = new Date()
+            })}
         default:
             return state
     }
